f085dc034a64633b67dd2e08787bddc7001205eb
Nicolas Mendez <nicomenfede@gmail.com>
2020-06-13 13:47:51 +0100

    First commit

---
+++ MergeNames/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class MergeNames
+{
+    public static string[] UniqueNames(string[] names1, string[] names2)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void Main(string[] args)
+    {
+        string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
+        string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
+        Console.WriteLine(string.Join(", ", MergeNames.UniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+    }
+}
